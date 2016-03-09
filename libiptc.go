4628456b4b3aa07a78ccf28f3aa637b515ffe3b2
gdm85 <gdm85@users.noreply.github.com>
2016-03-09 12:42:38 +0100

    Version bump 0.2.1

--- libiptc.go
+++ libiptc.go
@@ -1,5 +1,5 @@
 /*
- * go-libiptc v0.2.0 - libiptc bindings for Go language
+ * go-libiptc v0.2.1 - libiptc bindings for Go language
  * Copyright (C) 2015~2016 gdm85 - https://github.com/gdm85/go-libiptc/
 
 This program is free software; you can redistribute it and/or
